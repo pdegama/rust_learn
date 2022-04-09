dac1808aa80a8135aec7e59fc3dca73b1170118d
elparthka <parthka.2005@gmail.com>
2022-04-09 16:04:35 +0530

    add new wnv

--- src/server.rs
+++ src/server.rs
@@ -2,22 +2,15 @@ use std::convert::Infallible;
 use std::net::{SocketAddr};
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
-mod webres;
+pub mod webres;
 
-pub fn run_server(ip_add:[u8; 4], port: u16) {
+pub async fn run_server(ip_add:[u8; 4], port: u16) {
 
     async fn wer_res(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
         Ok(Response::new(webres::r1().into()))
     }
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
 
-
-
-            // We'll bind to 127.0.0.1:<port>
+            // We'll bind to <ip_add>:<port>
             let addr = SocketAddr::from((ip_add, port));
 
             // A `Service` is needed for every connection, so this
@@ -33,5 +26,4 @@ pub fn run_server(ip_add:[u8; 4], port: u16) {
             if let Err(e) = server.await {
                 eprintln!("server error: {}", e);
             }
-        })
 }
---
+++ src/userres/res2.rs
@@ -0,0 +1,3 @@
+pub fn main_res1(){
+    println!("res3")
+}
