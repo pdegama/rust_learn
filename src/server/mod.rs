6ff47fecccaf9d7f949f104b11e4d98cf307ce44
elparthka <parthka.2005@gmail.com>
2022-04-12 21:18:18 +0530

    itani khushi muje ajtak nahi hui

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,15 +1,17 @@
 use std::convert::Infallible;
 use std::net::{SocketAddr};
+use std::sync::{Arc, Mutex};
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 
 pub mod webres;
 
 pub async fn run_server(ip_add: [u8; 4], port: u16) {
-    let user_name = 42;
 
-    async fn wer_res(_req: Request<Body>, c: i32) -> Result<Response<Body>, Infallible> {
-        println!("{}", c);
+    async fn wer_res(_req: Request<Body>, c: Arc<Mutex<i32>>) -> Result<Response<Body>, Infallible> {
+        //access variable to thread
+        *c.lock().unwrap() += 1;
+        //println!("{:?}", c);
         Ok(Response::new(webres::r1().into()))
     }
 
@@ -19,8 +21,9 @@ pub async fn run_server(ip_add: [u8; 4], port: u16) {
     // A `Service` is needed for every connection, so this
     // creates one from our `wer_res` function.
     let make_svc = make_service_fn(|_conn| async {
+        let db = Arc::new(Mutex::new(49));
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(move |req| wer_res(req, 5465)))
+        Ok::<_, Infallible>(service_fn(move |req| wer_res(req, db.clone())))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
